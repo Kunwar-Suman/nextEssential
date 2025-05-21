5cd53da3cccada4aa7f7c4c039c9df2903bb4514
Suman Kunwar <sumankunwar8848@gmail.com>
2025-05-21 12:52:04 +0545

    added components folder for reusuable UI

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,11 @@
+import Layout from "../../components/Layout";
 export default function AboutPage() {
   return (
-    <main style={{ padding: "2rem" }}>
+    <Layout>
       <h1>About Me</h1>
       <p>
         Hi! I'm Suman. I'm a software developer exploring the world of web technologies with Next.js.
       </p>
-    </main>
+    </Layout>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+return (
+    <nav className={styles.navbar}>
+        <Link href="/" className={styles.link}>Home</Link> | 
+        <Link href="/about" className={styles.link}>About</Link> | 
+        <Link href="/blog" className={styles.link}>Blog</Link> | 
+        <Link href="/projects" className={styles.link}>Projects</Link> | 
+        <Link href="/contact" className={styles.link}>Contact</Link>
+    </nav>
+);
+}
