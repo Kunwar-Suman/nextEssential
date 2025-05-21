ba00d3605eec82abedad09f01dfdd023256b9b5e
Suman Kunwar <sumankunwar8848@gmail.com>
2025-05-21 15:20:05 +0545

    added more features in routes and structured folder

---
+++ src/app/contact/email/page.js
@@ -0,0 +1,8 @@
+export default function EmailPage() {
+  return (
+    <>
+      Email:
+      <a href="mailto:sumankunwar8848@gmail.com">sumankunwar8848@gmail.com</a>
+    </>
+  );
+}
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,15 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function ContactPage() {
   return (
     <Layout>
-      <h1>Contact</h1>
-      <p>If you'd like to get in touch, feel free to reach me out via email. And let's connect on LinkedIn.</p>
+      <h1>Contact Me</h1>
+      <Link href="/contact/email">Email Here</Link>
+      <p>
+        If you'd like to get in touch, feel free to reach me out via email. And
+        let's connect on LinkedIn.
+      </p>
     </Layout>
   );
 }
