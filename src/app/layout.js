77f04eec0d0e11e517b230c0a82b492d29f3fdf2
Suman Kunwar <sumankunwar8848@gmail.com>
2025-05-22 10:35:04 +0545

    module structure feature has been improvised

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Suman Portfolio",
+  description: "Welcome to my website",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
